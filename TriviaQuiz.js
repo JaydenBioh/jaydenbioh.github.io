3faf5604109c8ee02070ceff889a2572f0a88251
JaydenBioh <35264029+JaydenBioh@users.noreply.github.com>
2018-01-18 13:18:12 -0500

    Add files via upload

---
+++ TriviaQuiz.js
@@ -0,0 +1,11 @@
+var score = 0 
+var i = 1
+var ans = 1
+function submitAnswer(){
+    y = document.getElementById("qans").value
+    var yy=y.toUpperCase();
+    x = document.getElementById("question").value
+if (ans===1){
+    y = document.getElementById("qans").value
+    if(yy==="C"){
+        document.getElementById("question").value="You are right"
